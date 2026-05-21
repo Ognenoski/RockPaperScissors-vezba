7702572402ae09f674965d0abbf5e6387c17d4d9
Dani <ognenoski777dani@gmail.com>
2026-05-21 17:12:03 +0200

    update

--- script.js
+++ script.js
@@ -15,39 +15,44 @@ rock.addEventListener("click", () => {
     const option = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     console.log(randomNumber);
-    const computerChoice = option[randomNumber];
-    console.log(computerChoice);
-    if (userChoice === computerChoice) {
-        score.innerText = "DRAW!";
-        score.style.color = "#ff9100";
-    }
-    else if (
-        userChoice === "rock" && computerChoice === "scissors" ||
-        userChoice === "paper" && computerChoice === "rock" ||
-        userChoice === "scissors" && computerChoice === "paper"
-    ) {
-        score.innerText = "YOU WIN!";
-        score.style.color = "lime";
-        userScore++;
-        userScoreText.innerText = userScore;
+    score.innerText = "COMPUTER THINKING...";
+    score.style.color = "white";
+    setTimeout(() => {
+
+        const computerChoice = option[randomNumber];
+
+        if (userChoice === computerChoice) {
+            score.innerText = "DRAW!";
+            score.style.color = "#ff9100";
+        }
+        else if (
+            userChoice === "rock" && computerChoice === "scissors" ||
+            userChoice === "paper" && computerChoice === "rock" ||
+            userChoice === "scissors" && computerChoice === "paper"
+        ) {
+            score.innerText = "YOU WIN!";
+            score.style.color = "lime";
+            userScore++;
+            userScoreText.innerText = userScore;
 
 
-    }
-    else {
-        score.innerText = "COMPUTER WIN!";
-        score.style.color = "#ff3131";
-        computerScore++;
-        computerScoreText.innerText = computerScore;
-    }
-    if (userScore === 5) {
-        alert("YOU WON THE GAME!");
-    }
+        }
+        else {
+            score.innerText = "COMPUTER WIN!";
+            score.style.color = "#ff3131";
+            computerScore++;
+            computerScoreText.innerText = computerScore;
+        }
+        if (userScore === 5) {
+            alert("YOU WON THE GAME!");
+        }
 
-    if (computerScore === 5) {
-        alert("COMPUTER WON THE GAME!");
-    }
-    const computer = document.querySelector(".ComputerWin");
-    computer.style.backgroundImage = `url("${computerChoice}.png")`;
+        if (computerScore === 5) {
+            alert("COMPUTER WON THE GAME!");
+        }
+        const computer = document.querySelector(".ComputerWin");
+        computer.style.backgroundImage = `url("${computerChoice}.png")`;
+    }, 2000);
 });
 const paper = document.querySelector(".CardPaper");
 
@@ -58,38 +63,43 @@ paper.addEventListener("click", () => {
     const option = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     console.log(randomNumber);
-    const computerChoice = option[randomNumber];
-    console.log(computerChoice);
-    if (userChoice === computerChoice) {
-        score.innerText = "DRAW!";
-        score.style.color = "#ff9100";
-    }
-    else if (
-        userChoice === "rock" && computerChoice === "scissors" ||
-        userChoice === "paper" && computerChoice === "rock" ||
-        userChoice === "scissors" && computerChoice === "paper"
-    ) {
-        score.innerText = "YOU WIN!";
-        score.style.color = "lime";
-        userScore++;
-        userScoreText.innerText = userScore;
-    }
-    else {
-        score.innerText = "COMPUTER WIN!";
-        score.style.color = "#ff3131";
-        computerScore++;
-        computerScoreText.innerText = computerScore;
-    }
-    if (userScore === 5) {
-        alert("YOU WON THE GAME!");
-    }
+    score.innerText = "COMPUTER THINKING...";
+    score.style.color = "white";
+    setTimeout(() => {
 
-    if (computerScore === 5) {
-        alert("COMPUTER WON THE GAME!");
-    }
-    const computer = document.querySelector(".ComputerWin");
-    computer.style.backgroundImage = `url("${computerChoice}.png")`;
+        const computerChoice = option[randomNumber];
+        console.log(computerChoice);
+        if (userChoice === computerChoice) {
+            score.innerText = "DRAW!";
+            score.style.color = "#ff9100";
+        }
+        else if (
+            userChoice === "rock" && computerChoice === "scissors" ||
+            userChoice === "paper" && computerChoice === "rock" ||
+            userChoice === "scissors" && computerChoice === "paper"
+        ) {
+            score.innerText = "YOU WIN!";
+            score.style.color = "lime";
+            userScore++;
+            userScoreText.innerText = userScore;
+        }
+        else {
+            score.innerText = "COMPUTER WIN!";
+            score.style.color = "#ff3131";
+            computerScore++;
+            computerScoreText.innerText = computerScore;
+        }
+        if (userScore === 5) {
+            alert("YOU WON THE GAME!");
+        }
 
+        if (computerScore === 5) {
+            alert("COMPUTER WON THE GAME!");
+        }
+        const computer = document.querySelector(".ComputerWin");
+        computer.style.backgroundImage = `url("${computerChoice}.png")`;
+
+    }, 2000)
 });
 const scissors = document.querySelector(".CardScissors");
 
@@ -100,40 +110,45 @@ scissors.addEventListener("click", () => {
     const option = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     console.log(randomNumber);
-    const computerChoice = option[randomNumber];
-    console.log(computerChoice);
-    if (userChoice === computerChoice) {
-        score.innerText = "DRAW!";
-        score.style.color = "#ff9100";
-    }
-    else if (
-        userChoice === "rock" && computerChoice === "scissors" ||
-        userChoice === "paper" && computerChoice === "rock" ||
-        userChoice === "scissors" && computerChoice === "paper"
-    ) {
-        score.innerText = "YOU WIN!";
-        score.style.color = "#39ff14";
-        userScore++;
-        userScoreText.innerText = userScore;
+    score.innerText = "COMPUTER THINKING...";
+    score.style.color = "white";
+    setTimeout(() => {
+
+        const computerChoice = option[randomNumber];
+        console.log(computerChoice);
+        if (userChoice === computerChoice) {
+            score.innerText = "DRAW!";
+            score.style.color = "#ff9100";
+        }
+        else if (
+            userChoice === "rock" && computerChoice === "scissors" ||
+            userChoice === "paper" && computerChoice === "rock" ||
+            userChoice === "scissors" && computerChoice === "paper"
+        ) {
+            score.innerText = "YOU WIN!";
+            score.style.color = "#39ff14";
+            userScore++;
+            userScoreText.innerText = userScore;
 
 
-    }
-    else {
-        score.innerText = "COMPUTER WIN!";
-        score.style.color = "#ff3131";
-        computerScore++;
-        computerScoreText.innerText = computerScore;
-    }
-    if (userScore === 5) {
-        alert("YOU WON THE GAME!");
-    }
+        }
+        else {
+            score.innerText = "COMPUTER WIN!";
+            score.style.color = "#ff3131";
+            computerScore++;
+            computerScoreText.innerText = computerScore;
+        }
+        if (userScore === 5) {
+            alert("YOU WON THE GAME!");
+        }
 
-    if (computerScore === 5) {
-        alert("COMPUTER WON THE GAME!");
-    }
-    const computer = document.querySelector(".ComputerWin");
-    computer.style.backgroundImage = `url("${computerChoice}.png")`;
+        if (computerScore === 5) {
+            alert("COMPUTER WON THE GAME!");
+        }
+        const computer = document.querySelector(".ComputerWin");
+        computer.style.backgroundImage = `url("${computerChoice}.png")`;
 
+    }, 2000)
 });
 const restart = document.querySelector(".restart");
 
